d4e224ca791873b23017aaa0e0758360c64774a1
kuroneko <kskkm0228@gmail.com>
2019-10-09 18:02:15 +0900

    CRUDを汎用ビューで実装する

--- app/django_app/models.py
+++ app/django_app/models.py
@@ -1,7 +1,12 @@
 from django.db import models
+from django.urls import reverse
 
 # Create your models here.
 class Sample(models.Model):
     name = models.CharField(max_length=100)
     mail = models.CharField(max_length=100)
     age = models.IntegerField()
+
+    def get_absolute_url(self):
+        
+        return reverse('django_app:create')
--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -1,6 +1,6 @@
 from django.urls import path
 from .  import views
-
+app_name = 'django_app'
 urlpatterns = [
     path('', views.SampleCreateView.as_view(), name='create'),
     path('next', views.NextView.as_view(), name='next'),
