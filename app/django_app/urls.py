45077b5536eb33b3cb3f3582c41e233b97cf9db0
kuroneko <kskkm0228@gmail.com>
2019-10-09 14:43:06 +0900

    汎用ビューはアドリブがお上手

--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from .  import views
 
 urlpatterns = [
-    path('<int:id>/<name>/', views.index, name='index'),
+    path('<int:id>/<name>/', views.IndexView.as_view(), name='index'),
 ]
--- app/django_app/views.py
+++ app/django_app/views.py
@@ -1,12 +1,19 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from django.views import generic
 # Create your views here.
 
-def index(request, id, name):
-    params = {
-        'id': id,
-        'name': name,
-    }
-    result = 'あなたのidは' + str(id) + 'あなたの名前は' + name + "です。"
 
-    return render(request, 'django_app/index.html', params)
+class IndexView(generic.TemplateView):
+    template_name = 'django_app/index.html'
+    
+        
+
+# def index(request, id, name):
+#     params = {
+#         'id': id,
+#         'name': name,
+#     }
+#     result = 'あなたのidは' + str(id) + 'あなたの名前は' + name + "です。"
+
+#     return render(request, 'django_app/index.html', params)
