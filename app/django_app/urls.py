f69494f93ad5e7d03aef693e6f81fc68c975ec0a
kuroneko <kskkm0228@gmail.com>
2019-10-09 15:12:17 +0900

    URLのテンプレートタグにクエリパラメータを渡す

--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -3,5 +3,5 @@ from .  import views
 
 urlpatterns = [
     path('<int:id>/<name>/', views.IndexView.as_view(), name='index'),
-    path('next/', views.NextView.as_view(), name='next'),
+    path('next<int:id>/<name>', views.NextView.as_view(), name='next'),
 ]
