64b5f89d2688342df4c15d40d96796d08a5fb13a
kuroneko <kskkm0228@gmail.com>
2019-10-09 14:33:09 +0900

    テンプレートに変数を埋め込む

--- app/django_app/views.py
+++ app/django_app/views.py
@@ -3,6 +3,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request, id, name):
+    params = {
+        'id': id,
+        'name': name,
+    }
     result = 'あなたのidは' + str(id) + 'あなたの名前は' + name + "です。"
 
-    return HttpResponse(result)
+    return render(request, 'django_app/index.html', params)
