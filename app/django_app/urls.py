f84eccc678c9b59e3ad7012e7b2ce32f3ecb5112
kuroneko <kskkm0228@gmail.com>
2019-10-14 15:56:56 +0900

    難しい・・・

--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -1,9 +1,10 @@
-from django.urls import path, url
+from django.urls import path
 from .  import views
+from django.conf.urls import url
 app_name = 'django_app'
 urlpatterns = [
     path('', views.SampleCreateView.as_view(), name='create'),
     path('next', views.NextView.as_view(), name='next'),
-    url(r'^connection/','formView', name = 'loginform'),
-    url(r'^login/', 'login', name = 'login')
+    url(r'^connection/',views.formView, name = 'loginform'),
+    url(r'^login/', views.login, name = 'login')
 ]
