9f105549f6fc7579772510c16e0fa1e04f2b7525
kuroneko <kskkm0228@gmail.com>
2019-10-15 18:26:56 +0900

    ログイン画面をつくる

--- app/django_app/views.py
+++ app/django_app/views.py
@@ -22,42 +22,7 @@ class NextView(generic.ListView):
     model = Sample
     template_name = 'django_app/next.html'
 
-def login(request):
-   username = "not logged in"
-   
-   if request.method == "POST":
-      #Get the posted form
-        MyLoginForm = LoginForm(request.POST)
-   if request.method =='GET':
-       MyLoginForm = LoginForm()
 
-   if MyLoginForm.is_valid():
-        username = MyLoginForm.cleaned_data['username']
-   else:
-        MyLoginForm = LoginForm()
-   
-   response = render_to_response(request, 'loggedin.html', {"username" : username}, context_instance = RequestContext(request))
-   
-   response.set_cookie('last_connection', datetime.datetime.now())
-   response.set_cookie('username', datetime.datetime.now())
-	
-   return response
-
-def formView(request):
-   if 'username' in request.COOKIES and 'last_connection' in request.COOKIES:
-      username = request.COOKIES['username']
-      
-      last_connection = request.COOKIES['last_connection']
-      last_connection_time = datetime.datetime.strptime(last_connection[:-7], 
-         "%Y-%m-%d %H:%M:%S")
-      
-      if (datetime.datetime.now() - last_connection_time).seconds < 10:
-         return render(request, 'loggedin.html', {"username" : username})
-      else:
-         return render(request, 'login.html', {})
-			
-   else:
-      return render(request, 'login.html', {})
 # def index(request, id, name):
 #     params = {
 #         'id': id,
