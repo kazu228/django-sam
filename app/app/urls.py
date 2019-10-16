ec5bd5cfd025d286a837826ae1ad98544e79e2dc
kuroneko <kskkm0228@gmail.com>
2019-10-16 16:16:00 +0900

    ミドルウェアを使って、ユーザーにログインを必須にする

--- app/app/urls.py
+++ app/app/urls.py
@@ -18,5 +18,5 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('django_app/', include('django_app.urls')),
+    path('login/', include('django_app.urls')),
 ]
--- app/django_app/urls.py
+++ app/django_app/urls.py
@@ -3,7 +3,7 @@ from .  import views
 from django.conf.urls import url
 app_name = 'django_app'
 urlpatterns = [
-    path('', views.SampleCreateView.as_view(), name='create'),
+    # path('', views.SampleCreateView.as_view(), name='create'),
     path('next', views.NextView.as_view(), name='next'),
     # url(r'^connection/',views.formView, name = 'loginform'),
     # url(r'^login/', views.login, name = 'login'),
@@ -11,6 +11,6 @@ urlpatterns = [
     path('user_data_input/', views.user_data_input, name='user_data_input'),
     path('user_data_confirm/', views.user_data_confirm, name='user_data_confirm'),
     path('user_data_create/', views.user_data_create, name='user_data_create'),
-    path('login/', views.Login.as_view(), name='login'),
+    path('', views.Login.as_view(), name='login'),
     path('logout/', views.Logout.as_view(), name='logout'),
 ]
