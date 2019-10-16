a8414a53b2a6ad8305dc458ecce9253a3abe655b
kuroneko <kskkm0228@gmail.com>
2019-10-16 18:29:21 +0900

    add post_comment.html

--- app/django_app/views.py
+++ app/django_app/views.py
@@ -121,4 +121,4 @@ def post_comment(request):
             return HttpResponse("You've already commented")
         request.session['has_commented'] = True
         return HttpResponse('Thanks for your comment !!')
-    return render_to_response('post_commnet.html')
+    return render_to_response('post_comment.html')
